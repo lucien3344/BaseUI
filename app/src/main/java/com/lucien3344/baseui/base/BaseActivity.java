64ed335bb9cae7b3e24c4f951da747b93716ecdf
lucien3344 <lsh881127>
2018-09-07 18:08:30 +0800

    第2次提交

---
+++ app/src/main/java/com/lucien3344/baseui/base/BaseActivity.java
@@ -0,0 +1,30 @@
+package com.lucien3344.baseui.base;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.lucien3344.baseui.R;
+
+import butterknife.OnClick;
+
+public abstract class BaseActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(this);
+    }
+
+
+
+
+}
